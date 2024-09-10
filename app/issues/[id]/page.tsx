ad7915e5be7cf13842247eb509c08bda8f683fb6
nizar7zakout <nizar7zakout@gmail.com>
2024-09-10 20:12:35 +0300

    add the eidt button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,10 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge"
 import { prisma } from "@/prisma/client"
-import { Box, Card, Flex, Heading, Text } from "@radix-ui/themes"
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes"
 import { notFound } from "next/navigation"
 import ReactMarkdown from 'react-markdown'
-
+import { Pencil2Icon } from '@radix-ui/react-icons'
+import Link from "next/link"
 
 const IssueDetailsPage = async ( { params: { id } }: { params: { id: string } } ) => {
 
@@ -17,16 +18,26 @@ const IssueDetailsPage = async ( { params: { id } }: { params: { id: string } } 
         notFound()
 
     return (
-        <Box>
-            <Heading>{ issue.title }</Heading>
-            <Flex gap='4' my='3'>
-                <IssueStatusBadge status={ issue.status } />
-                <Text>{ issue.createdAt.toDateString() }</Text>
-            </Flex>
-            <Card className="prose">
-                <ReactMarkdown>{ issue.description }</ReactMarkdown>
-            </Card>
-        </Box>
+        <Grid columns={ { initial: "1", sm: "2" } } gap='5'>
+            <Box>
+                <Heading>{ issue.title }</Heading>
+                <Flex gap='4' my='3'>
+                    <IssueStatusBadge status={ issue.status } />
+                    <Text>{ issue.createdAt.toDateString() }</Text>
+                </Flex>
+                <Card className="prose">
+                    <ReactMarkdown>{ issue.description }</ReactMarkdown>
+                </Card>
+            </Box>
+            <Box>
+                <Button>
+                    <Pencil2Icon />
+                    <Link href={ `/issues/${issue.id}/edit` }>
+                        Edit Issue
+                    </Link>
+                </Button>
+            </Box>
+        </Grid>
     )
 }
 
