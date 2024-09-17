696f2f20d2d087f5eb083e78f5acc4a7ede9c46e
nizar7zakout <nizar7zakout@gmail.com>
2024-09-17 09:40:50 +0300

    remove duplicate skeletons

--- app/NabBar.tsx
+++ app/NabBar.tsx
@@ -8,7 +8,7 @@ const NabBar = () => {
     const pathName = usePathname()
     const links = [
         { label: "Dashboard", href: "/" },
-        { label: "Issues", href: "/issues" },
+        { label: "Issues", href: "/issues/list" },
     ]
 
     return (
