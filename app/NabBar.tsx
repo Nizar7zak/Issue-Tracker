e2a28bac0a744f35c2bfa7735e2c9eefe33ddd72
nizar7zakout <nizar7zakout@gmail.com>
2024-09-18 08:33:04 +0300

    add the login and logout links

--- app/NabBar.tsx
+++ app/NabBar.tsx
@@ -1,11 +1,14 @@
 "use client"
+import { Box } from "@radix-ui/themes";
 import classNames from "classnames";
 import Link from "next/link"
 import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
+import { useSession } from 'next-auth/react'
 
 const NabBar = () => {
     const pathName = usePathname()
+    const { status, data: session } = useSession()
     const links = [
         { label: "Dashboard", href: "/" },
         { label: "Issues", href: "/issues/list" },
@@ -17,21 +20,25 @@ const NabBar = () => {
                 <FaBug />
             </Link>
             <ul className="flex space-x-6">
-                { links.map( ( link ) => <Link
-                    key={ link.href }
-                    href={ link.href }
-                    className={
-                        classNames( {
-                            "text-zinc-900": link.href === pathName,
-                            "text-zinc-500": link.href !== pathName,
-                            "hover:text-zinc-800 transition-colors": true,
-                        } )
-                    }>
-                    { link.label }
-                </Link>
-
+                { links.map( ( link ) => <li key={ link.href }>
+                    <Link
+                        href={ link.href }
+                        className={
+                            classNames( {
+                                "text-zinc-900": link.href === pathName,
+                                "text-zinc-500": link.href !== pathName,
+                                "hover:text-zinc-800 transition-colors": true,
+                            } )
+                        }>
+                        { link.label }
+                    </Link>
+                </li>
                 ) }
             </ul>
+            <Box>
+                { status === "unauthenticated" && <Link href='/api/auth/signin'>Login</Link> }
+                { status === "authenticated" && <Link href='/api/auth/signout'>Logout</Link> }
+            </Box>
         </nav>
     )
 }
