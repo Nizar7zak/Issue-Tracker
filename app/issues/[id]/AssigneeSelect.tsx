adbaa9f19400a18a121cfa11a4c5a261f18c9ce8
nizar7zakout <nizar7zakout@gmail.com>
2024-09-19 07:07:27 +0300

    Assign an issue to user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,12 +1,11 @@
 "use client"
-import { User } from '@prisma/client'
+import { Skeleton } from '@/app/components'
+import { Issue, User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import { Skeleton } from '@/app/components'
 
-const AssigneeSelect = () => {
-
+const AssigneeSelect = ( { issue }: { issue: Issue } ) => {
     const { data: users, error, isLoading } = useQuery( {
         queryKey: [ 'users' ],
         queryFn: () => axios.get<User[]>( '/api/users' ).then( res => res.data ),
@@ -17,14 +16,28 @@ const AssigneeSelect = () => {
     if ( isLoading ) return <Skeleton height='2rem' />
     if ( error ) return null;
 
+    const assignToUser = async ( userId: string | null ) => {
+        if ( userId === "Unassigned" )
+            userId = null
+        await axios.patch(
+            `/api/issues/${issue.id}`,
+            { assignedToUserId: userId }
+        )
+    }
     return (
-        <Select.Root>
-            <Select.Trigger placeholder='Assign...' />
+        <Select.Root
+            defaultValue={ issue.assignedToUserId || "Unassigned" }
+            onValueChange={ ( value ) => assignToUser( value ) }
+        >
+            <Select.Trigger />
             <Select.Content>
                 <Select.Group>
                     <Select.Label>
                         Suggestions
                     </Select.Label>
+                    <Select.Item value="Unassigned">
+                        Unassigned
+                    </Select.Item>
                     { users?.map( ( user ) => <Select.Item
                         key={ user.id }
                         value={ user.id }
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -30,7 +30,7 @@ const IssueDetailsPage = async ( { params: { id } }: { params: { id: string } } 
                 session &&
                 <Box>
                     <Flex direction='column' gap='4'>
-                        <AssigneeSelect />
+                        <AssigneeSelect issue={ issue } />
                         <EditIssueButton issueId={ issue.id } />
                         <DeleteIssueButton issueId={ issue.id } />
                     </Flex>
