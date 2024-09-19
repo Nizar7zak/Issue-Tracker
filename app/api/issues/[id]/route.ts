e579dd581ad1e0c758f9f8eaf1b41bf1f360e100
nizar7zakout <nizar7zakout@gmail.com>
2024-09-19 03:29:31 +0300

    implement the API assign issue to user

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import { prisma } from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -9,34 +9,44 @@ export const PATCH = async (
     reqeust: NextRequest,
     { params: { id } }: { params: { id: string } }
 ) => {
+
     const session = await getServerSession( authOptions )
     if ( !session )
         return NextResponse.json( {}, { status: 401 } )
 
     const body = await reqeust.json()
-    const validation = issueSchema.safeParse( body )
+    const validation = patchIssueSchema.safeParse( body )
 
     if ( !validation.success )
         return NextResponse.json( validation.error.format(), { status: 400 } )
+
+    const { title, description, assignedToUserId, status } = body
+
+    if ( assignedToUserId ) {
+        const user = await prisma.user.findUnique( {
+            where: { id: assignedToUserId }
+        } )
+        if ( !user )
+            return NextResponse.json( { error: "Invalid user ID" }, { status: 400 } )
+    }
 
     const issue = await prisma.issue.findUnique( {
         where: {
             id: parseInt( id )
         }
     } )
-
     if ( !issue )
-        return NextResponse.json( { error: 'Invalid Issue ' }, { status: 400 } )
+        return NextResponse.json( { error: 'Invalid Issue ' }, { status: 404 } )
 
     const updatedIssue = await prisma.issue.update( {
         where: {
             id: issue.id
         },
         data: {
-            title: body.title,
-            description: body.description,
-            status: body.status
-
+            title,
+            description,
+            assignedToUserId,
+            status,
         }
     } )
     return NextResponse.json( updatedIssue )
@@ -57,7 +67,7 @@ export const DELETE = async (
     } )
 
     if ( !issue )
-        return NextResponse.json( { error: 'Invalid isse' }, { status: 404 } )
+        return NextResponse.json( { error: 'Invalid issue' }, { status: 404 } )
 
     await prisma.issue.delete( {
         where: {
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -2,7 +2,12 @@ import z from 'zod'
 
 export const issueSchema = z.object( {
     title: z.string().min( 1 ).max( 255 ),
-    description: z.string().min( 1 ),
-    status: z.enum( [ 'OPEN', 'CLOSED', 'IN_PROGRESS' ] )
+    description: z.string().min( 1 ).max( 65535 ),
 } )
 
+export const patchIssueSchema = z.object( {
+    title: z.string().min( 1 ).max( 255 ).optional(),
+    description: z.string().min( 1 ).max( 65535 ).optional(),
+    assignedToUserId: z.string().min( 1, "Assigned to user id is required" ).max( 255 ).optional().nullable(),
+    status: z.enum( [ 'OPEN', 'CLOSED', 'IN_PROGRESS' ] ).optional()
+} )
