82472f0115a94bbd7cb1ce2cf8eeae44789129a6
nizar7zakout <nizar7zakout@gmail.com>
2024-09-19 11:21:39 +0300

    handle invalid query

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -23,15 +23,21 @@ const IssuesPage = async ( { searchParams }: Props ) => {
         ]
 
     const statuses = Object.values( STATUS )
+
     const status = statuses.includes( searchParams.status ) ?
         searchParams.status :
         undefined;
+
+    const orderBy = columns.map( ( col ) => col.value )
+        .includes( searchParams.orderBy ) ?
+        { [ searchParams.orderBy ]: "asc" } :
+        undefined
 
     const issues = await prisma.issue.findMany( {
         where: {
             status
         },
-        orderBy: searchParams.orderBy ? { [ searchParams.orderBy ]: "asc" } : undefined
+        orderBy
     } )
 
     return (
