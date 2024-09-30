5a8ae72babf8734856fbefa06d6bdb3eedb0d38a
nizar7zakout <nizar7zakout@gmail.com>
2024-09-30 22:48:21 +0300

    build project page

--- app/projects/list/page.tsx
+++ app/projects/list/page.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { prisma } from "@/prisma/client"
+import { Card, Flex, Grid, Heading, Text } from "@radix-ui/themes"
 
-const ProjectsPage = () => {
+const ProjectsPage = async () => {
+  const projects = await prisma.project.findMany( {
+    where: {}
+  } )
   return (
-    <div>ProjectsPage</div>
+    <>
+      <Heading mb='5'>Projects:</Heading>
+
+      <Grid columns={ { initial: '1', md: '2', lg: '3' } } gapX="9" gapY='5' width="auto" >
+        { projects.map( ( project ) => <Card
+          style={ { padding: 0 } }
+          key={ project.id }
+        >
+          <Flex direction='column' align='start'>
+            <Heading as="h4" className="bg-violet-100 w-full text-center py-3" >{ project.title }</Heading>
+            <Text className="my-4 ml-4">dd{ project.description }</Text>
+          </Flex>
+        </Card> ) }
+      </Grid>
+    </>
   )
 }
 
