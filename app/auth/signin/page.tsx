39cb302ec8ee21660a88bfe9052fe65a2eba5fbc
nizar7zakout <nizar7zakout@gmail.com>
2024-10-16 11:44:09 +0300

    style login page

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -3,12 +3,13 @@ import ErrorCallOut from "@/app/components/Form/ErrorCallOut";
 import FormTitle from "@/app/components/Form/FormTitle";
 import { authSchema } from "@/app/validationSchemas";
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Heading, Spinner } from "@radix-ui/themes";
+import { Button, Flex, Heading, Separator, Spinner } from "@radix-ui/themes";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from 'zod';
+import { FaGoogle } from "react-icons/fa";
 
 type SignInData = z.infer<typeof authSchema>
 
@@ -43,43 +44,53 @@ const SignIn = () => {
     } )
 
     return (
-        <div>
-            <Heading>Sign In</Heading>
-            <Button onClick={ () => {
-                signIn( 'google', {
-                    callbackUrl: '/issues/list'
-                } )
-            }
-            }>
-                Sign in with Google
-            </Button>
-
-            <form
-                className="space-y-3"
-                onSubmit={ onSubmit }
-            >
-                <FormTitle
-                    placeholder="Email"
-                    register={ register( "email" ) }
-                    error={ errors.email }
-                />
-                <FormTitle
-                    placeholder="Password"
-                    register={ register( "password" ) }
-                    error={ errors.password }
-                    type="password"
-                />
-                <Button
-                    type="submit"
-                    disabled={ isSubmitting }
+        <Flex
+            className="mt-20"
+            justify="center"
+        >
+            <Flex className="w-2/5 rounded-2xl bg-indigo-100 p-12" direction='column' gap='3'>
+                <Heading className="self-center text-indigo-800">Sign In</Heading>
+                <form
+                    className="space-y-3 flex flex-col"
+                    onSubmit={ onSubmit }
                 >
-                    Sign in with Credentials
-                    { isSubmitting && <Spinner /> }
-                </Button>
-                { error && <ErrorCallOut error={ error } /> }
+                    <FormTitle
+                        placeholder="Email"
+                        register={ register( "email" ) }
+                        error={ errors.email }
+                    />
+                    <FormTitle
+                        placeholder="Password"
+                        register={ register( "password" ) }
+                        error={ errors.password }
+                        type="password"
+                    />
+                    <Button
+                        type="submit"
+                        className="self-stretch"
+                        disabled={ isSubmitting }
+                    >
+                        Sign in with Credentials
+                        { isSubmitting && <Spinner /> }
+                    </Button>
+                    { error && <ErrorCallOut error={ error } /> }
 
-            </form>
-        </div>
+                </form>
+                <Separator
+                    className="my-0.5 bg-indigo-700"
+                    style={ { height: '2px', width: '100%' } }
+                />
+                <Button onClick={ () => {
+                    signIn( 'google', {
+                        callbackUrl: '/issues/list'
+                    } )
+                }
+                }>
+                    <FaGoogle />
+                    Sign in with Google
+                </Button>
+            </Flex>
+        </Flex>
     );
 };
 
